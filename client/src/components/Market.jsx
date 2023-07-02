256835a1d4c472deacf04b2378c095fa3611bc0d
New User <anjaliss.2022@gmail.com>
2023-07-02 19:57:58 +0530

    real time data getting fetched

--- client/src/components/Market.jsx
+++ client/src/components/Market.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const Market = () => {
+function Market() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Market
+      <h2>Market Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Last</th>
+            <th>Vol</th>
+            <th>Open</th>
+            <th>High</th>
+            <th>Low</th>
+            <th>Close</th>
+            <th>Bid</th>
+            <th>Ask</th>
+            <th>Time</th>
+            <th>Prev close</th>
+            <th>Change</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td className="text-white">{item['Trading Symbol']}</td>
+              <td className="text-white">{item['Last Traded Price (LTP)']}</td>
+              <td className="text-white">{item['Volume']}</td>
+              <td className="text-white">{item['Open']}</td>
+              <td className="text-white">{item['High']}</td>
+              <td className="text-white">{item['Low']}</td>
+              <td className="text-white">{item['Close']}</td>
+              <td className="text-white">{item['Bid Price']}</td>
+              <td className="text-white">{item['Ask Price']}</td>
+              <td className="text-white">{item['Timestamp']}</td>
+              <td className="text-white">{item['Previous Close Price']}</td>
+              <td className="text-white">{item['Change']}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Market
+export default Market;
--- server/app.py
+++ server/app.py
@@ -1,84 +1,100 @@
 import os
 import csv
 import socket
+import struct
 import threading
 from flask import Flask, jsonify
 from flask_cors import CORS
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 app = Flask(__name__)
 CORS(app)
 
-# Set the path to the data folder
 data_folder = os.path.join(os.path.dirname(__file__), 'data')
-
-# Set the path to the dataset.csv file
 dataset_file = os.path.join(data_folder, 'dataset.csv')
 
-# Define the TCP/IP server configuration
-SERVER_HOST = '127.0.0.1'  # Replace with your desired server IP
-SERVER_PORT = 9011  # Replace with your desired server port
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 9011
 
-# Create a lock to synchronize access to the dataset
 data_lock = threading.Lock()
-
-# Create a list to store the latest dataset
 latest_data = []
 
+packet_format = '<I30sQLQLQLQLQLQQ'
 
-def load_dataset():
-    """Load data from the dataset.csv file."""
-    global latest_data
+field_descriptions = [
+    ('Packet Length', 'Int32', 0, 4),
+    ('Trading Symbol', 'String', 4, 30),
+    ('Sequence Number', 'Long/int64', 34, 8),
+    ('Timestamp', 'Long/Int64', 42, 8),
+    ('Last Traded Price (LTP)', 'Long/Int64', 50, 8),
+    ('Last Traded Quantity', 'Long/Int64', 58, 8),
+    ('Volume', 'Long/Int64', 66, 8),
+    ('Bid Price', 'Long/Int64', 74, 8),
+    ('Bid Quantity', 'Long/Int64', 82, 8),
+    ('Ask Price', 'Long/Int64', 90, 8),
+    ('Ask Quantity', 'Long/Int64', 98, 8),
+    ('Open Interest (OI)', 'Long/Int64', 106, 8),
+    ('Previous Close Price', 'Long/Int64', 114, 8),
+    ('Previous Open Interest', 'Long/Int64', 122, 8)
+]
 
+@app.route('/')
+def get_latest_data():
+    with data_lock:
+        return jsonify(latest_data)
+
+def process_packet(packet):
+    with data_lock:
+        fields = struct.unpack(packet_format, packet)
+        packet_data = {
+            'Packet Length': fields[0],
+            'Trading Symbol': fields[1].decode().strip('\x00'),
+            'Sequence Number': fields[2],
+            'Timestamp': fields[3],
+            'Last Traded Price (LTP)': fields[4],
+            'Last Traded Quantity': fields[5],
+            'Volume': fields[6],
+            'Bid Price': fields[7],
+            'Bid Quantity': fields[8],
+            'Ask Price': fields[9],
+            'Ask Quantity': fields[10],
+            'Open Interest (OI)': fields[11],
+            'Previous Close Price': fields[12],
+            'Previous Open Interest': fields[13]
+        }
+        latest_data.append(packet_data)
+
+def process_data_file():
     with open(dataset_file, 'r') as file:
-        csv_reader = csv.DictReader(file)
-        latest_data = list(csv_reader)
+        reader = csv.reader(file)
+        for row in reader:
+            packet = struct.pack(packet_format, *row)
+            process_packet(packet)
 
+def start_watchdog():
+    class FileModifiedHandler(FileSystemEventHandler):
+        def on_modified(self, event):
+            if not event.is_directory and event.src_path == dataset_file:
+                process_data_file()
 
-def handle_client(client_socket):
-    """Handle a client connection."""
-    while True:
-        # Receive a single byte packet from the client
-        packet = client_socket.recv(1)
-
-        if packet:
-            # Acquire the data lock to access the dataset
-            with data_lock:
-                # Send the latest dataset to the client
-                response = jsonify(latest_data)
-                client_socket.sendall(response.encode())
-
-    client_socket.close()
-
+    event_handler = FileModifiedHandler()
+    observer = Observer()
+    observer.schedule(event_handler, data_folder, recursive=False)
+    observer.start()
 
 def start_server():
-    """Start the TCP/IP server."""
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((SERVER_HOST, SERVER_PORT))
-    server_socket.listen()
-
-    print(f'Server listening on {SERVER_HOST}:{SERVER_PORT}')
+    server_socket.listen(1)
 
     while True:
-        client_socket, client_address = server_socket.accept()
-        print(f'Client connected: {client_address}')
-
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
-
-
-# Route to fetch the latest dataset
-@app.route('/', methods=['GET'])
-def get_data():
-    return jsonify(latest_data)
-
+        client_socket, _ = server_socket.accept()
+        packet = client_socket.recv(256)
+        process_packet(packet)
+        client_socket.close()
 
 if __name__ == '__main__':
-    # Load the dataset from the CSV file
-    load_dataset()
-
-    # Start the TCP/IP server in a separate thread
-    server_thread = threading.Thread(target=start_server)
-    server_thread.start()
-
-    # Run the Flask application
-    app.run(debug=True)
+    threading.Thread(target=start_server).start()
+    threading.Thread(target=start_watchdog).start()
+    app.run(host='0.0.0.0', port=8080)
